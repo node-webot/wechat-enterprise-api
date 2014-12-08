13043565b6ddb6172204d8325530cbe7edb679a0
Jackson Tian <shyvo1987@gmail.com>
2014-12-08 23:10:40 +0800

    Init project

---
+++ index.js
@@ -0,0 +1,15 @@
+var API = require('./lib/api_common');
+// 部门管理
+API.mixin(require('./lib/api_department'));
+// 媒体管理（上传、下载）
+API.mixin(require('./lib/api_media'));
+// 菜单管理
+API.mixin(require('./lib/api_menu'));
+// 消息发送
+API.mixin(require('./lib/api_message'));
+// 标签管理
+API.mixin(require('./lib/api_tag'));
+// 用户管理
+API.mixin(require('./lib/api_user'));
+
+module.exports = API;
---
+++ test/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  corpid: 'wx4e2c2b771c467c9f',
+  corpsecret: 'k7TGD8xJLDU6-sPH3NwY0eTs2oBPyAINMdbSbGN80fuEt01UK0Z8dWzhm7crgkz7',
+  token: 'mRoQySqj2XBEORdnuOh9wei17',
+  encodingAESKey: '6mRfWp9o1dfXHnmRBCFmTlpM3IIY377wy2iDJJjx4lM'
+};
